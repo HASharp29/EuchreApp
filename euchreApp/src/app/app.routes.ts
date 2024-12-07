2949aa415f4250e083c8541918fac7564bcf5876
Noelle Haviland <neh8@calvin.edu>
2024-12-07 14:45:47 -0500

    Added Board component and routing

--- euchreApp/src/app/app.routes.ts
+++ euchreApp/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
+import { BoardComponent } from './components/board/board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: GameComponent },
+  { path: 'board', component: BoardComponent }
+];
---
+++ euchreApp/src/app/components/board/board.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { GameService, Game } from '../../services/game.service';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-board',
+  imports: [RouterLink],
+  standalone: true,
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.css'
+})
+export class BoardComponent {
+  gameService = inject(GameService);
+  game: Game = this.gameService.initializeGame(["John Calvin", "Dancing Baby", "Norman", "Rickroll Astley"]);
+  players = this.gameService.initializeGame(["John Calvin", "Dancing Baby", "Norman", "Rickroll Astley"]).players;
+  currentPlayer = this.players[0];
+  cards = 0;
+}
