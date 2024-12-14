643f1826545f1642d969d52e56a51fa44944d1f7
Emma Wang <seekerofmail@gmail.com>
2024-12-14 17:05:28 -0500

    pause button on board

--- euchreApp/src/app/app.routes.ts
+++ euchreApp/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { BoardComponent } from './components/board/board.component';
 import { StartScreenComponent } from './components/start-screen/start-screen.component';
+import { PauseComponent } from './components/pause/pause.component';
 
 export const routes: Routes = [
   { path: '', component: StartScreenComponent },
-  { path: 'board', component: BoardComponent }
+  { path: 'board', component: BoardComponent },
+  { path: 'pause', component: PauseComponent},
+
+  { path: '**', redirectTo: 'board'},
 ];
